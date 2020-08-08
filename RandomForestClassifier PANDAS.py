cda0a43654787cabf6d9414bd5fa5c3120d7cb46
e-ember <69371417+e-ember@users.noreply.github.com>
2020-08-08 14:58:21 -0700

    Update RandomForestClassifier PANDAS.py

--- RandomForestClassifier PANDAS.py
+++ RandomForestClassifier PANDAS.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-doc = pd.read_csv("/Users/ember/OneDrive/Desktop/County data without spaces.csv")
+doc = pd.read_csv("/Users/ember/OneDrive/Desktop/EXAMPLE county data without spaces NOT COMPLETELY ACCURATE DATA.csv")
 a_names = np.array(doc.pop('Classes Example'))
 
 train, test, train_labels, test_labels = train_test_split(doc, a_names, stratify = a_names, test_size = 0.3, random_state = 60)
